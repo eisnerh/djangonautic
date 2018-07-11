00f25e9f15f2b7df69a1c7785cd591437c9c13f1
Eisner López Acevedo <elopeza@ucatolica.ac.cr>
2018-07-11 13:03:51 -0600

    details html

--- articles/views.py
+++ articles/views.py
@@ -9,4 +9,9 @@ from .models import Article
 def article_list(request):
     articles = Article.objects.all().order_by("date")
 
-    return render(request, "articles/articles_list.html", {'articles':articles})
+    return render(request, "articles/articles_list.html", {'articles': articles})
+
+
+def article_details(request, pk):
+    details = Article.objects.get(id=pk)
+    return render(request, "articles/details.html", {'details': details})
