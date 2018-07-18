058a9071ed21723eb05431d5868a8eb1427cac1b
Eisner López Acevedo <elopeza@ucatolica.ac.cr>
2018-07-18 12:52:36 -0600

    Login y SignUp

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from accounts.views import sign_up_views
+from accounts.views import sign_up_views, login_views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', sign_up_views, name='sing_up'),
+    path('login/', login_views, name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 
 
@@ -16,3 +16,14 @@ def sign_up_views(request):
     else:
         form = UserCreationForm()
     return render(request, 'accounts/sing_up.html', {'form': form})
+
+
+def login_views(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('articles:article_list')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'accounts/login.html', {'form': form})
